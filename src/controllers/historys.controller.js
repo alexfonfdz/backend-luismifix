7c3b8fd079057d78bf3764f8b4fa8e4ca0fa1111
alexfonfdz <fofapersonal@gmail.com>
2024-04-19 12:36:22 -0700

    Arreglo en los search de los historicos y inclusión de datos  a retornar en más detalles

--- src/controllers/historys.controller.js
+++ src/controllers/historys.controller.js
@@ -39,18 +39,24 @@ export const searchInHistoryProductsForNameProductUserDate = async (req, res) =>
 
         let query = {};
         if (nameProduct) {
-            const product = await Product.findOne({nameProduct: nameProduct});
+            const product = await Product.findOne({nameProduct: {$regex: nameProduct, $options: 'i'}});
+            if (!product) return res.status(404).json({message: 'Producto no encontrado'});
             query.idProduct = product._id;
         }
         if (nameUser) {
-            const user = await User.findOne({username: nameUser});
+            const user = await User.findOne({username: {$regex: nameUser, $options: 'i'}});
+            if (!user) return res.status(404).json({message: 'Usuario no encontra'});
             query.idUser = user._id;
         }
         if (date) {
-            query.date = date;
+            const [year, month, day] = date.split('-');
+            const startOfDay = new Date(Date.UTC(year, month - 1, day, 0, 0, 0, 0));
+            const endOfDay = new Date(Date.UTC(year, month - 1, day, 23, 59, 59, 999));
+            query.dateMovement = { $gte: startOfDay, $lte: endOfDay };
         }
 
         const historyProducts = await HistoryProduct.find(query).populate('idProduct', 'nameProduct').populate('idMovementType', 'nameMovementType').populate('idUser', 'username');
+        if (historyProducts.length === 0) return res.status(404).json({message: 'No se encontraron productos en el historial con esos datos'});
         return res.status(200).json(historyProducts);
 
     }catch(error){
@@ -64,23 +70,25 @@ export const searchInHistoryPurchasesForNameProductUserDate = async (req, res) =
 
         let query = {};
         if (nameProduct) {
-            const product = await Product.findOne({nameProduct: nameProduct});
-            if (product) {
-                const purchases = await Purchase.find({idProduct: product._id});
-                query.idPurchase = { $in: purchases.map(purchase => purchase._id) };
-            }
+            const product = await Product.findOne({nameProduct: {$regex: nameProduct, $options: 'i'}});
+            if (!product) return res.status(404).json({message: 'Producto no encontrado'});
+            const purchases = await Purchase.find({idProduct: product._id});
+            query.idPurchase = { $in: purchases.map(purchase => purchase._id) };
         }
         if (nameUser) {
-            const user = await User.findOne({username: nameUser});
-            if (user) {
-                query.idUser = user._id;
-            }
+            const user = await User.findOne({username: {$regex: nameUser, $options: 'i'}});
+            if (!user) return res.status(404).json({message: 'Usuario no encontrado'});
+            query.idUser = user._id;
         }
         if (date) {
-            query.date = date;
+            const [year, month, day] = date.split('-');
+            const startOfDay = new Date(Date.UTC(year, month - 1, day, 0, 0, 0, 0));
+            const endOfDay = new Date(Date.UTC(year, month - 1, day, 23, 59, 59, 999));
+            query.date = { $gte: startOfDay, $lte: endOfDay };
         }
 
         const historyPurchases = await HistoryPurchases.find(query).populate('idPurchase').populate('idMovementType', 'nameMovementType').populate('idUser', 'username');
+        if (historyPurchases.length === 0) return res.status(404).json({message: 'No se encontraron compras en el historial con esos datos'});
         return res.status(200).json(historyPurchases);
 
     }catch(error){
@@ -94,22 +102,24 @@ export const searchInHistoryProvidersForNameProviderUserDate = async (req, res) 
 
         let query = {};
         if (nameProvider) {
-            const provider = await Provider.findOne({nameProvider: nameProvider});
-            if (provider) {
-                query.idProvider = provider._id;
-            }
+            const provider = await Provider.findOne({nameProvider: {$regex: nameProvider, $options: 'i'}});
+            if (!provider) return res.status(404).json({message: 'Proveedor no encontrado'});
+            query.idProvider = provider._id;
         }
         if (nameUser) {
-            const user = await User.findOne({username: nameUser});
-            if (user) {
-                query.idUser = user._id;
-            }
+            const user = await User.findOne({username: {$regex: nameUser, $options: 'i'}});
+            if (!user) return res.status(404).json({message: 'Usuario no encontrado'});
+            query.idUser = user._id;
         }
         if (date) {
-            query.date = date;
+            const [year, month, day] = date.split('-');
+            const startOfDay = new Date(Date.UTC(year, month - 1, day, 0, 0, 0, 0));
+            const endOfDay = new Date(Date.UTC(year, month - 1, day, 23, 59, 59, 999));
+            query.date = { $gte: startOfDay, $lte: endOfDay };
         }
 
         const historyProviders = await HistoryProvider.find(query).populate('idProvider', 'nameProvider').populate('idMovementType', 'nameMovementType').populate('idUser', 'username');
+        if (historyProviders.length === 0) return res.status(404).json({message: 'No se encontraron proveedores en el historial con esos datos'});
         return res.status(200).json(historyProviders);
 
     }catch(error){
@@ -121,7 +131,8 @@ export const moreDetailsHistoryProduct = async (req, res) => {
     try{
         const {id} = req.params;
         const historyProduct = await HistoryProduct.findById(id).populate('idProduct', 'nameProduct').populate('idMovementType', 'nameMovementType').populate('idUser', 'username');
-        return res.status(200).json(historyProduct);
+        const product = await Product.findById(historyProduct.idProduct).populate('idTypeProduct', 'nameTypeProduct');
+        return res.status(200).json({historyProduct, product});
     }catch(error){
         return res.status(500).json({message: error.message});
     }
@@ -132,7 +143,7 @@ export const moreDetailsHistoryPurchase = async (req, res) => {
         const {id} = req.params;
         const historyPurchase = await HistoryPurchases.findById(id).populate('idPurchase').populate('idMovementType', 'nameMovementType').populate('idUser', 'username');
         const purchase = await Purchase.findById(historyPurchase.idPurchase).populate('idProduct', 'nameProduct').populate('idProvider', 'nameProvider');
-        return res.status(200).json(historyPurchase, purchase);
+        return res.status(200).json({historyPurchase, purchase});
     }catch(error){
         return res.status(500).json({message: error.message});
     }
@@ -142,7 +153,8 @@ export const moreDetailsHistoryProvider = async (req, res) => {
     try{
         const {id} = req.params;
         const historyProvider = await HistoryProvider.findById(id).populate('idProvider', 'nameProvider').populate('idMovementType', 'nameMovementType').populate('idUser', 'username');
-        return res.status(200).json(historyProvider);
+        const provider = await Provider.findById(historyProvider.idProvider);
+        return res.status(200).json({historyProvider, provider});
     }catch(error){
         return res.status(500).json({message: error.message});
     }
