13a8ee8ac90aafe1b87490e6f3cd93a8d329f2f4
alexfonfdz <fofapersonal@gmail.com>
2024-04-19 11:33:39 -0700

    Error al validar admin

--- src/middlewares/validateTokenAdmin.js
+++ src/middlewares/validateTokenAdmin.js
@@ -7,11 +7,11 @@ export const authRequiredAdmin = (req, res, next) => {
 
     if(!token) return res.status(401).json({message: "Acceso denegado, no token"});
 
-    jwt.verify(token, SECRET, (err, user) => {
+    jwt.verify(token, SECRET, async (err, user) => {
         if(err) return res.status(403).json({message: "Acceso denegado"});
-
-        const typeUser = TypeUser.findById(user.idTypeUser);
-
+        
+        const typeUser = await TypeUser.findById(user.idTypeUser);
+        
         if(!typeUser) return res.status(404).json({message: "Tipo de usuario desconocido"});
 
         if(typeUser.nameTypeUser !== "Administrador") return res.status(403).json({message: "Acceso denegado, no eres administrador"});
