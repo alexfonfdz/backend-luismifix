1c25d746665e12aedf4e6e0b9f8e3b75c4ee646f
alexfonfdz <fofapersonal@gmail.com>
2024-04-19 12:45:07 -0700

    Aumento en expiración de la cookie

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 export function createAccessToken(payload){
     return new Promise((resolve, reject) => {
-        jwt.sign(payload, SECRET, {expiresIn: '15m'}, (err, token) => {
+        jwt.sign(payload, SECRET, {expiresIn: '2h'}, (err, token) => {
             if(err) reject(err);
             resolve(token);
         });
